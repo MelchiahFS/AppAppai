0bcce2ae857bc83bd73fe89a11b6e693c4de52c0
Melchiahfs <melchiah91@gmail.com>
2018-02-13 13:24:58 +0100

    App d'esempio

---
+++ app/src/main/java/ium/appappai/Appappai.java
@@ -0,0 +1,13 @@
+package ium.appappai;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Appappai extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appappai);
+    }
+}
